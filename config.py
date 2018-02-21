2d0ea06f21fba5623e3b3b8222b65790f4070b40
Paul <jinyy0822@163.com>
2018-02-21 18:21:54 +0800

    u

---
+++ config.py
@@ -0,0 +1,3 @@
+config = dict()
+config['shadowsocks_log_path'] = "/var/log"
+config['current_path'] = "/root/checking_for_ip"
--- ip_checker.py
+++ ip_checker.py
@@ -1,34 +1,49 @@
+# -*- coding:utf-8 -*-
+
+
 import datetime
+import pygeoip
+from config import config
+import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
+gic = pygeoip.GeoIP('GeoLiteCity.dat')
+
+
+def get_country_and_city(ip_address):
+    global gic
+    # noinspection PyBroadException
+    try:
+        res = gic.record_by_addr(ip_address)['country_name'] + ', ' + gic.record_by_addr(ip_address)['city']
+    except:
+        res = 'Unknown'
+    return res
 
 
 # Read the white list
 ip_white_list = set()
-# with open("ip_white_list.csv") as f:
-with open("/root/checking_for_ip/ip_white_list.csv") as f:
+with open(config['current_path'] + "/ip_white_list.csv") as f:
     for line in f:
-        ip_white_list.add(str(line).split('\r')[0])
-print 'ip_white_list: ', list(ip_white_list), '\r\n'
+        ip_white_list.add(get_country_and_city(str(line).split('\r')[0]))
 
 
 # check all the ip
 ip_pool = set()
-# with open("shadowsocks.log") as f:
-with open("/var/log/shadowsocks.log") as f:
+with open(config['shadowsocks_log_path'] + "/shadowsocks.log") as f:
     for line in f:
         list_of_content = str(line).split(' ')
-        if list_of_content[2] == 'INFO' and list_of_content[-2] == 'from':
+        content_datetime = datetime.datetime.strptime(list_of_content[0] + list_of_content[1], '%Y-%m-%d%H:%M:%S')
+        if list_of_content[2] == 'INFO' and list_of_content[-2] == 'from' and \
+                (datetime.datetime.now() - content_datetime).days <= 1:
             ip = list_of_content[-1].split(':')[0]
             ip_pool.add(ip)
-print 'ip_have_accessed: ', list(ip_pool), '\r\n'
 
 
 # check for unusual ip
 for ip in list(ip_pool):
-    if ip not in ip_white_list:
-        print'Nowtime in Beijing: ' + str(datetime.datetime.utcnow()
-                                                         + datetime.timedelta(hours=8)) + '\r\n'
-        print'Found unusual IP Address: ' + ip + '\r\n'
-        with open("/root/checking_for_ip/log.txt", 'a') as fl:
-            fl.write('Nowtime in Beijing: ' + str(datetime.datetime.utcnow()
-                                                 + datetime.timedelta(hours=8)) + '\r\n')
-            fl.write('Found unusual IP Address: ' + ip + '\r\n')
+    if get_country_and_city(ip) not in ip_white_list:
+        print 'Unusual access within 1 day: ' + ip + ' from ' + get_country_and_city(ip) + '\r\n'
+
+
+print 'Whitelist of cities: ', list(ip_white_list), '\r\n'
+print 'Access within 1 day: ', list(ip_pool), '\r\n'
