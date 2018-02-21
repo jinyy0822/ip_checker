51660b182593a295d51ada28d4f9fb613b174ac1
jinyy0822 <jinyy0822@163.com>
2018-02-21 21:24:54 +0800

    u

--- test.py
+++ test.py
@@ -1,10 +1,12 @@
+import datetime
 from config import config
 
 with open(config['shadowsocks_log_path'] + "/shadowsocks.log") as f:
     for line in f:
         list_of_content = str(line).split(' ')
         try:
-            list_of_content[0] + list_of_content[1]
+            content_datetime = datetime.datetime.strptime(list_of_content[0] + list_of_content[1], '%Y-%m-%d%H:%M:%S')
         except:
             print line
             print list_of_content
+            print list_of_content[0] + list_of_content[1]
